29980c2e988104182629b1db4fb98ed326ecd273
Jonah Kirangi <jonahkirangi@gmail.com>
2013-12-13 11:36:06 -0800

    Start of an example Rails app, to show how to make a Tweetster.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tweetster::Application.config.secret_key_base = 'b55f3cd2ff28ede7d44074a86230665c51fcb8819efe233865d46265cae064b74691d8cf543a8d4ff63e817cb057cb0079fe27e26dee989f7f4f2966313f6bfb'
